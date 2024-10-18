dc396a361060c3b2d457345ff09456e81b30e770
saraaels <saramohamedelsaharty32@gmail.com>
2024-10-18 09:49:49 +0300

    hash

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Prayer from './Prayer';
-import { HashRouter } from 'react-router-dom';
 
 
 
 function App() {
   return (
-
-     <HashRouter base="/">
+    <div className="App">
       <Prayer/>
- </HashRouter>
+    </div>
+   
   );
 }
 
