d9982d49caffc39770764bb5a7ad242e2bf7ed2c
saraaels <saramohamedelsaharty32@gmail.com>
2024-10-18 09:40:53 +0300

    sec commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Prayer from './Prayer';
+import { HashRouter } from 'react-router-dom';
 
 
 
 function App() {
   return (
-    <div className="App">
+
+     <HashRouter base="/">
       <Prayer/>
-    </div>
+ </HashRouter>
   );
 }
 
