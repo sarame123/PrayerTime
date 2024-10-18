f2cba52f9b784069457eaee03c9a04cab621789c
saraaels <saramohamedelsaharty32@gmail.com>
2024-10-18 10:30:57 +0300

    l

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter>
-    <App />
+    <App/>
   </HashRouter>
 );
 
